c8092342f30278384f42a952d2f2fdb4665f015b
Agustin Barrero <agustinbarrero5k@gmail.com>
2023-03-11 01:46:08 -0300

    Desafio 2 re hecho correctamente, implemente file system y los metodos ya estan hechos y funcionan correctamente.

--- desafio-dos/ProductManager.js
+++ desafio-dos/ProductManager.js
@@ -2,6 +2,7 @@
     Comision: 51135
     Desafio: 2
 */
+const fs = require('fs');
 class Product{
     constructor(title, description, price, thumbnail, code, stock ){
         this.title = title;
@@ -12,79 +13,104 @@ class Product{
         this.stock = stock;
     }
 }
+
 class ProductManager {
-    constructor(products, path){
-      this.products = products;
-      this.path = path
-      path = 'data.json'
+    constructor(){
+      this.path = 'data.json';
       this.currentId = 1;
+      this.products = [];
+      try {
+        const data = fs.readFileSync(this.path,'utf-8');
+        if(data.length === 0){
+          this.products = []
+        }else{
+          this.products = JSON.parse(data)
+        }
+        this.currentId = this.products.length + 1;
+      } catch (error) {
+        console.error('Error al leer el archivo', error);
+      }
     }
   
-    addProduct(product) {
-        // Verificar si ya existe un producto con la misma ID
-        this.products.forEach((existingProduct) => {
-          if (existingProduct.code === product.code) {
-            throw new Error('Ya existe un producto con la misma ID');
-          }
-        });
+    async addProduct(product) {
+      // Verificar si ya existe un producto con la misma ID
+      this.products.forEach((existingProduct) => {
+        if (existingProduct.code === product.code) {
+          throw new Error('Ya existe un producto con la misma ID');
+        }
+      });
       
-        // Agregandole el id autoincrementado al producto
-        const id = this.currentId++;
-        product.id = id;
-      
-        // Agregar el nuevo producto a la lista.
-        this.products.push(product);
+      // Agregandole el id autoincrementado al producto
+      const id = this.currentId++;
+      product.id = id;
+    
+      const productosJson = JSON.stringify([...this.products, product]); // crea una copia actualizada de this.products y agrega el nuevo producto
+      this.products.push(product);
+    
+      try {
+        await fs.promises.writeFile(this.path, productosJson);
+        console.log('Archivo creado satisfactoriamente.')
+      } catch (error) {
+        throw new Error('No se pudieron cargar los productos al archivo')
       }
-      
-    getProducts(){
+    }    
+
+    async getProducts(){
+      try {
+        const data = await fs.promises.readFile(this.path,'utf-8');
+        console.log(JSON.stringify(JSON.parse(data), null,2));
+      } catch (error) {
+        throw new Error('Error al leer el archivo')
+      }
       return this.products;
     }
-  
+
     getProductById(id){
       return this.products.find(product => product.code === id);
     }
 
-    updateProduct(id, updatedProduct) {
-        const productIndex = this.products.findIndex((product) => product.id === id);
-    
-        if (productIndex === -1) {
-          throw new Error(`No se encontró ningún producto con el ID ${id}`);
-        }
-    
-        const existingProduct = this.products[productIndex];
-    
-        // Si se proporciona un nuevo código, verificar si no se está intentando duplicar el código.
-        if (updatedProduct.code && updatedProduct.code !== existingProduct.code) {
-          const codeExists = this.products.some((product) => product.code === updatedProduct.code);
-          if (codeExists) {
-            throw new Error('Ya existe un producto con el mismo código');
-          }
-        }
-    
-        const updated = {
-          ...existingProduct,
-          ...updatedProduct,
-          id: existingProduct.id, // asegurarse de que el id no se borre, ni se cambie.
-        };
-    
-        this.products[productIndex] = updated;
+    async updateProduct(id, updateFields) {
+      const index = this.products.findIndex((product) => product.code === id);
+      if (index === -1) {
+        throw new Error(`No existe un producto con el id ${id}`);
       }
-    
-      deleteProduct(id) {
-        const productIndex = this.products.findIndex((product) => product.id === id);
-    
-        if (productIndex === -1) {
-          throw new Error(`No se encontró ningún producto con el ID ${id}`);
-        }
-    
-        this.products.splice(productIndex, 1);
+      const updatedProduct = {
+        ...this.products[index],
+        ...updateFields,
+        code: id
+      };
+      this.products[index] = updatedProduct;
+      const productosJson = JSON.stringify(this.products);
+      try {
+        await fs.promises.writeFile(this.path, productosJson);
+        console.log('Archivo actualizado satisfactoriamente.');
+      } catch (error) {
+        throw new Error('No se pudieron cargar los productos al archivo');
       }
+    }
+    
+    async deleteProduct(id) {
+      const index = this.products.findIndex((product) => product.code === id);
+      if (index === -1) {
+        throw new Error(`No existe un producto con el id ${id}`);
+      }
+
+      this.products.splice(index,1)
+      const productosJson = JSON.stringify(this.products);
+      try {
+        await fs.promises.truncate(this.path);
+        await fs.promises.writeFile(this.path, productosJson);
+        console.log('Producto eliminado correctamente')
+      } catch (error) {
+        throw new Error('No se pudieron cargar los productos al archivo')
+      }
+    }
 }      
   const products  = []
   const productoUno = new Product ('Glock 20','Pistola calibre 9x19mm modelo 20 Gen 4',581,'glock20.jpg',1,15)
   const productoDos = new Product ('Bersa BP9CC','Pistola calibre 9x19mm modelo BP9CC',299,'bersaBP9CC.jpg',2,15)
   const productoTres = new Product ('Ruger SR22P','Pistola calibre 22mm modelo SR22P',299,'rugerSR22P.jpg',3,15)
-  const productoCuatro = new Product ('Sig Sauer SIG 716','Carabina calibre 7.62 semiautomatica modelo SIG 716',1300,'sig716.jpg',4,15)
+  const productoCuatro = new Product ('Sig Sauer SIG 716','Carabina calibre 7.62 semiautomatica modelo SIG 716',13030,'sig716.jpg',4,15)
 
   const manager = new ProductManager(products)
 
@@ -93,22 +119,11 @@ class ProductManager {
   manager.addProduct(productoTres)
   manager.addProduct(productoCuatro)
 
-
-//Prueba del metodo deleteManager (Borrar comentario para hacer la prueba)
-
-//manager.deleteProduct(3)
-
-//Haciendo prueba del metodo updateProduct, agarro uno de los productos para actualizarlo...
-const productToUpdate = manager.getProductById(2);
-
-//Ahora actualizo el producto cambiando valores.
-productToUpdate.price = 305 //Nota: el valor inicial de price en el producto con id 2 era 299
-productToUpdate.stock = 10  //Nota: el valor inicial de stock en el producto con id 2 era 15
-
-//Y finalmente le paso al metodo updateProduct el producto actualizado
-manager.updateProduct(2,productToUpdate)
-
+  manager.updateProduct(2,{price:350})
+  manager.deleteProduct(4)
 //Imprimo en la consola los productos.
     console.log('-----------------------------')
     console.log(manager);
     console.log('-----------------------------')
+    //manager.getProducts()
+
