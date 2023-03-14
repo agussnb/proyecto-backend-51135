06e6a0886b06051e0ff7043fad80a0f33c56c48d
Agustin Barrero <agustinbarrero5k@gmail.com>
2023-03-13 21:52:32 -0300

    Hice cambios en los metodos getProducts y getProductById

--- desafio-dos/ProductManager.js
+++ desafio-dos/ProductManager.js
@@ -3,6 +3,7 @@
     Desafio: 2
 */
 const fs = require('fs');
+const path = require('path');
 class Product{
     constructor(title, description, price, thumbnail, code, stock ){
         this.title = title;
@@ -15,9 +16,11 @@ class Product{
 }
 
 class ProductManager {
-    constructor(){
-      this.path = 'data.json';
-      this.currentId = 1;
+  constructor(){
+    const dataPath = path.join(__dirname, 'data.json');
+    this.path = dataPath
+    this.currentId = 1;
+    if (!this.products) {  // Agregar esta condición para evitar el bucle infinito
       this.products = [];
       try {
         const data = fs.readFileSync(this.path,'utf-8');
@@ -31,45 +34,54 @@ class ProductManager {
         console.error('Error al leer el archivo', error);
       }
     }
+  }
   
-    async addProduct(product) {
-      // Verificar si ya existe un producto con la misma ID
-      this.products.forEach((existingProduct) => {
-        if (existingProduct.code === product.code) {
-          throw new Error('Ya existe un producto con la misma ID');
-        }
-      });
-      
-      // Agregandole el id autoincrementado al producto
-      const id = this.currentId++;
-      product.id = id;
-    
-      const productosJson = JSON.stringify([...this.products, product]);
-      this.products.push(product);
-    
-      try {
-        await fs.promises.writeFile(this.path, productosJson);
-        console.log('Archivo creado satisfactoriamente.')
-      } catch (error) {
-        throw new Error('No se pudieron cargar los productos al archivo')
+  async addProduct(product) {
+    // Verificar si ya existe un producto con la misma ID
+    this.products.forEach((existingProduct) => {
+      if (existingProduct.code === product.code) {
+        throw new Error('Ya existe un producto con la misma ID');
       }
-    }    
-
-    async getProducts(){
-      try {
-        //Leo el archivo json y obtengo los productos.
-        const data = await fs.promises.readFile(this.path,'utf-8');
-        console.log(JSON.stringify(JSON.parse(data), null,2));
-      } catch (error) {
-        throw new Error('Error al leer el archivo')
-      }
-      return this.products;
+    });
+  
+    // Agregandole el id autoincrementado al producto
+    const id = this.currentId++;
+    product.id = id;
+  
+    this.products.push(product);
+  
+    try {
+      const productosJson = JSON.stringify(this.products);
+      await fs.promises.writeFile(this.path, productosJson);
+      console.log('Archivo creado satisfactoriamente.')
+    } catch (error) {
+      throw new Error('No se pudieron cargar los productos al archivo')
     }
+  } 
 
-    getProductById(id){
-      return this.products.find(product => product.code === id);
+  async getProducts() {
+    try {
+      const data = await fs.promises.readFile(this.path, 'utf-8');
+      const products = JSON.parse(data);
+      return products; // Devuelve el array de productos original
+    } catch (error) {
+      throw new Error('Error al leer el archivo');
     }
+  }
+  
 
+  async getProductById(id) {
+    try {
+      const product = this.products.find((product) => product.code === id);
+      return product || null; // Devuelve null si no se encuentra el producto
+    } catch (error) {
+      throw error;
+    }
+  }
+  
+  
+  
+  
     async updateProduct(id, updateFields) {
       const index = this.products.findIndex((product) => product.code === id);
       if (index === -1) {
@@ -83,9 +95,9 @@ class ProductManager {
       };
       //Le paso al array el nuevo producto actualizado, luego se convierte en string para que pueda ser escrito con fs.
       this.products[index] = updatedProduct;
-      const productosJson = JSON.stringify(this.products);
-      //Se escribe el archivo json con el nuevo producto actualizado.
+    
       try {
+        const productosJson = JSON.stringify(this.products);
         await fs.promises.writeFile(this.path, productosJson);
         console.log('Archivo actualizado satisfactoriamente.');
       } catch (error) {
@@ -98,18 +110,18 @@ class ProductManager {
       if (index === -1) {
         throw new Error(`No existe un producto con el id ${id}`);
       }
-
+    
       this.products.splice(index,1) //Elimina del array el producto seleccionado por id usando el indice.
-      const productosJson = JSON.stringify(this.products);
+    
       try {
-        await fs.promises.truncate(this.path); //Elimina todo el contenido del archivo json.
-        await fs.promises.writeFile(this.path, productosJson);  //Vuelve a escribir el archivo json con la info nueva.
+        const productosJson = JSON.stringify(this.products);
+        await fs.promises.writeFile(this.path, productosJson);
         console.log('Producto eliminado correctamente')
       } catch (error) {
         throw new Error('No se pudieron cargar los productos al archivo')
       }
     }
-}      
+}     
   const products  = []
   const productoUno = new Product ('Glock 20','Pistola calibre 9x19mm modelo 20 Gen 4',581,'glock20.jpg',1,15)
   const productoDos = new Product ('Bersa BP9CC','Pistola calibre 9x19mm modelo BP9CC',299,'bersaBP9CC.jpg',2,15)
@@ -118,17 +130,32 @@ class ProductManager {
 
   const manager = new ProductManager(products)
 
-  manager.addProduct(productoUno) //Agrego el producto 1 
-  manager.addProduct(productoDos) //Agrego el producto 2
-  manager.addProduct(productoTres)  //Agrego el producto 3 
-  manager.addProduct(productoCuatro)  //Agrego el producto 4
+  const start = async () => {
+    try {
+      // Agregar productos
+      // await manager.addProduct(productoUno); Comentado porque: Ya esta agregado el producto
+      // await manager.addProduct(productoDos); Comentado porque: Ya esta agregado el producto
+      // await manager.addProduct(productoTres);  Comentado porque: Ya esta agregado el producto
+      // await manager.addProduct(productoCuatro);  Comentado porque: Ya esta agregado el producto
+  
+      // Modificar producto
+      //await manager.updateProduct(2, { price: 350, stock: 10 });  Comentado porque: Ya esta actualizado el producto
+  
+      // Eliminar producto
+      // await manager.deleteProduct(1);  Comentado porque: No necesito borrar el producto
+      // await manager.deleteProduct(2);  Comentado porque: No necesito borrar el producto
+      // await manager.deleteProduct(3);  Comentado porque: No necesito borrar el producto
+      // await manager.deleteProduct(4);  Comentado porque: No necesito borrar el producto
+  
+    } catch (error) {
+      console.log('Este es el error: '+error.message);
+    }
+  };
 
-  manager.updateProduct(2,{price:350, stock:10})  //Modifico el producto con id 2
-  manager.deleteProduct(4) //Borro el producto con id 4 del archivo y del array
+  start()
 //Imprimo en la consola los productos.
     console.log('-----------------------------')
     console.log(manager); 
     console.log('-----------------------------')
-    console.log(manager.getProducts())
-    console.log('-----------------------------')
-    console.log(manager.getProductById(2));
+
+    module.exports = ProductManager;
