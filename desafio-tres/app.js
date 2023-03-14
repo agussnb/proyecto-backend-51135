9ce340755a82f27337fbcb9769841ab62c93f60f
Agustin Barrero <agustinbarrero5k@gmail.com>
2023-03-13 21:49:46 -0300

    Empece el servidor, la parte de los endpoints con query?limit esta terminada, falta la parte que usa req.params para que el endpoint /products/:pid funcione, ya que no estoy logrando que getProductById me devuelva el producto.

---
+++ desafio-tres/app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const ProductManager = require('../desafio-dos/ProductManager');
+
+const app = express();
+const PORT = 8080;
+
+// Creamos una instancia de ProductManager
+
+
+app.use(express.urlencoded({ extended: true }));
+
+const productManager = new ProductManager()
+
+app.get('/',(req,res)=>{
+  res.send('<h1>Endpoint /</h1>')
+})
+app.get('/products', async (req, res) => {
+  const { title, price, stock, limit } = req.query;
+
+  const products = await productManager.getProducts();
+
+  if (!Array.isArray(products)) {
+    // Si products no es un array, envía un error en la respuesta
+    return res.status(500).send('Error en el servidor');
+  }
+
+  let matchingProducts = products;
+
+  if (title || price || stock) {
+    matchingProducts = matchingProducts.filter((product) => {
+      if (title && product.title !== title) return false;
+      if (price && product.price !== Number(price)) return false;
+      if (stock && product.stock !== Number(stock)) return false;
+      return true;
+    });
+  }
+
+  if (limit) {
+    matchingProducts = matchingProducts.slice(0, limit);
+  }
+
+  res.json(matchingProducts);
+});
+
+app.get('/products/:pid', async (req, res) => {
+  const productId = req.params.pid;
+  console.log('Id del producto: ' + productId);
+  try {
+    const product = await productManager.getProductById(productId);
+    console.log(product);
+    if (!product) {
+      return res.status(404).send('Producto no encontrado');
+    }
+    res.send(`El producto seleccionado es: ${JSON.stringify(product)}`);
+  } catch (error) {
+   // console.log('error '+error)
+   // console.error(error);
+    res.status(500).send('Error en el servidor');
+  }
+});
+
+
+// Listen del port
+app.listen(PORT, () => {
+  console.log(`Server running in port:${PORT}`);
+});
